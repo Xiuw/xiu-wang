c25f4057c2e91d73f1f970bf1e9308cdfe2a9a33
xiuw <sylvi.xw@gmail.com>
2019-04-07 01:29:24 -0400

    Updates

---
+++ precache-manifest.6478d1e25cd3574f4ed8760fb4575bab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "94de7c6cba7c6340368dbba5d93cdb7e",
+    "url": "/static/media/xiu.94de7c6c.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9253416824c62bd13e73",
+    "url": "/static/js/main.50934ecb.chunk.js"
+  },
+  {
+    "revision": "2680e64b5bd34d5ceae6",
+    "url": "/static/js/2.5e92f5a1.chunk.js"
+  },
+  {
+    "revision": "9253416824c62bd13e73",
+    "url": "/static/css/main.e8faa8bf.chunk.css"
+  },
+  {
+    "revision": "2680e64b5bd34d5ceae6",
+    "url": "/static/css/2.8d255d30.chunk.css"
+  },
+  {
+    "revision": "1d4402ab70e0607977bb754bdb624580",
+    "url": "/index.html"
+  }
+];
