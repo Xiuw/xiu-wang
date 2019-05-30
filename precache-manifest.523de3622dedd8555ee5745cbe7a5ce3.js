6414437137fa4f3d05557208cefe26b8e89f5155
xiuw <sylvi.xw@gmail.com>
2019-05-29 20:14:59 -0400

    Updates

---
+++ precache-manifest.523de3622dedd8555ee5745cbe7a5ce3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "94de7c6cba7c6340368dbba5d93cdb7e",
+    "url": "/xiu-wang/static/media/xiu.94de7c6c.jpg"
+  },
+  {
+    "revision": "f3a65edb8489c2439ff02924a2cd932f",
+    "url": "/xiu-wang/static/media/Wang_2019.f3a65edb.pdf"
+  },
+  {
+    "revision": "040037f4e645a635c0f9",
+    "url": "/xiu-wang/static/js/runtime~main.07f03688.js"
+  },
+  {
+    "revision": "19a3d3489c938f27cb54",
+    "url": "/xiu-wang/static/js/main.afea8431.chunk.js"
+  },
+  {
+    "revision": "63db616aac4ccd509b5a",
+    "url": "/xiu-wang/static/js/2.6f7e367b.chunk.js"
+  },
+  {
+    "revision": "19a3d3489c938f27cb54",
+    "url": "/xiu-wang/static/css/main.ad62debc.chunk.css"
+  },
+  {
+    "revision": "63db616aac4ccd509b5a",
+    "url": "/xiu-wang/static/css/2.78de8034.chunk.css"
+  },
+  {
+    "revision": "95aaff2489b0a4b8a87e293caee1b926",
+    "url": "/xiu-wang/index.html"
+  }
+];
