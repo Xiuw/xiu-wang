9b5e1c55d5459f3ba6df7e175f1602c4e144265d
xiuw <sylvi.xw@gmail.com>
2019-05-31 10:51:37 -0400

    hamberger bar

--- src/components/Navigation/SideNav.js
+++ src/components/Navigation/SideNav.js
@@ -9,7 +9,7 @@ const SideNav = ({handleSideNav, showHideNav}) =>{
 		<div>
 			<Animated animationIn="jello" isVisible={true} >
 				<div className={showHideNav? "hidebar" : "flex navbar"}  id="style-1">
-					<div className="bar pointer fixed z-max ml4 mt4 absolute" id="delBtn" onClick={handleSideNav}> 
+					<div className="bar pointer fixed z-999 ml3 mt3 absolute" id="delBtn" onClick={handleSideNav}> 
 		                 <i style={{background:"#ccac9d",borderRadius:"10px"}} 
 		                 	className="fas fa-bars pa3 f3 white-80 hover-mid-gray"></i>     
 		            </div>
